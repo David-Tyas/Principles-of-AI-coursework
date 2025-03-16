d94f9d5e8e473a9d6867c9899ec7261978fac1f0
David-Tyas <David.Tyas@city.ac.uk>
2025-03-16 16:55:12 +0000

    Generate JSON file with statistics

--- preprocess.py
+++ preprocess.py
@@ -19,6 +19,24 @@ def format_income(income):
 def format_house_value(house_value):
     return f"${(house_value * 100_000):.2f}"
 
+statistics = {
+    "Median income": {
+        "Max": format_income(np.max(median_incomes)),
+        "Min": format_income(np.min(median_incomes)),
+        "Mean": format_income(np.mean(median_incomes)),
+        "Median": format_income(np.median(median_incomes)),
+        "Standard deviation": format_income(np.std(median_incomes))
+    },
+    "Median house value": {
+        "Max": format_house_value(np.max(median_house_values)),
+        "Min": format_house_value(np.min(median_house_values)),
+        "Mean": format_house_value(np.mean(median_house_values)),
+        "Median": format_house_value(np.median(median_house_values)),
+        "Standard deviation": format_house_value(np.std(median_house_values))
+    }
+}
+with open("statistics.json", "w") as file:
+    json.dump(statistics, file)
 print(f"""Median income statistics:
 Max: {format_income(np.max(median_incomes))}
 Min: {format_income(np.min(median_incomes))}
@@ -32,7 +50,7 @@ Mean: {format_house_value(np.mean(median_house_values))}
 Median: {format_house_value(np.median(median_house_values))}
 Standard deviation: {format_house_value(np.std(median_house_values))}""")
 
-(training_median_incomes,
+'''(training_median_incomes,
 testing_median_incomes,
 training_median_house_values,
 testing_median_house_values) = train_test_split(median_incomes, median_house_values, train_size=0.2)
@@ -40,8 +58,8 @@ testing_median_house_values) = train_test_split(median_incomes, median_house_val
 def put_array_in_json_file(arr, file_name):
     with open(file_name, "w") as file:
         json.dump(arr.tolist(), file)
-'''put_array_in_json_file(features, "features.json")
-put_array_in_json_file(median_incomes, "median_incomes.json")'''
+put_array_in_json_file(features, "features.json")
+put_array_in_json_file(median_incomes, "median_incomes.json")
 put_array_in_json_file(training_median_incomes, "training_median_incomes.json")
 training_median_incomes.dump("training_median_incomes.pickle")
 put_array_in_json_file(testing_median_incomes, "testing_median_incomes.json")
@@ -49,7 +67,7 @@ testing_median_incomes.dump("testing_median_incomes.pickle")
 put_array_in_json_file(training_median_house_values, "training_median_house_values.json")
 training_median_house_values.dump("training_median_house_values.pickle")
 put_array_in_json_file(testing_median_house_values, "testing_median_house_values.json")
-testing_median_house_values.dump("testing_median_house_values.pickle")
+testing_median_house_values.dump("testing_median_house_values.pickle")'''
 
 plt.style.use("_mpl-gallery")
 plt.scatter(median_incomes, median_house_values)
