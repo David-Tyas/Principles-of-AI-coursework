57269e2f5ee9acec7271875433abee27d43b638a
David-Tyas <David.Tyas@city.ac.uk>
2025-03-18 13:55:23 +0000

    Overlaid regression line on test data scatter plot

--- linear-regression.py
+++ linear-regression.py
@@ -6,9 +6,20 @@ Created on Sun Mar 16 13:25:11 2025
 """
 
 import sklearn.linear_model as linear
+import matplotlib.pyplot as plt
 import numpy as np
 
 training_median_incomes = np.load("training_median_incomes.pickle", allow_pickle=True)
 testing_median_incomes = np.load("testing_median_incomes.pickle", allow_pickle=True)
 training_median_house_values = np.load("training_median_house_values.pickle", allow_pickle=True)
 testing_median_house_values = np.load("testing_median_house_values.pickle", allow_pickle=True)
+
+linear_regression = linear.LinearRegression()
+linear_regression.fit(training_median_incomes.reshape(-1,1),training_median_house_values.reshape(-1,1))
+prediction = linear_regression.predict(testing_median_house_values.reshape(-1,1))
+#print(linear_regression.coef_)
+
+plt.style.use("_mpl-gallery")
+plt.scatter(testing_median_incomes, testing_median_house_values)
+
+plt.axline((0.0, linear_regression.intercept_[0]), (1.0, linear_regression.coef_[0][0] + linear_regression.intercept_[0]))
