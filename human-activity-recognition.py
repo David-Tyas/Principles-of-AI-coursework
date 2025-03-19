69f4769fe7ce6892733bdd0401965f3f0ccc39bd
David-Tyas <David.Tyas@city.ac.uk>
2025-03-19 17:02:41 +0000

    Attempt to fetch training features

--- human-activity-recognition.py
+++ human-activity-recognition.py
@@ -9,7 +9,32 @@ import sklearn.svm as svm
 import matplotlib.pyplot as plt
 import numpy as np
 
+training_xs = []
 activities = []
+
+with open(r".\UCI HAR dataset\train\X_train.txt", "r") as file:
+    while True:
+        line = file.readline()
+        if line == "":
+            break
+        assert line[-1] == "\n"
+        #Each line starts with two spaces,
+        #so we strip them off as well as the line feed.
+        line = line[2:-1]
+        training_xs.append([float(x) for x in line.split(" ")])
+
+with open(r".\UCI HAR dataset\train\subject_train.txt", "r") as file:
+    i = 0
+    while True:
+        line = file.readline()
+        if line == "":
+            break
+        assert line[-1] == "\n"
+        line = line[:-1]
+        assert i < len(training_xs)
+        training_xs[i].append(int(line))
+        i += 1
+print(training_xs)
 
 with open(r".\UCI HAR dataset\train\y_train.txt", "r") as file:
     while True:
