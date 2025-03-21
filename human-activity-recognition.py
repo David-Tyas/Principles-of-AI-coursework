a470f9975214dd938ecf122d9b8d5e31c8019ffa
David-Tyas <David.Tyas@city.ac.uk>
2025-03-21 11:05:21 +0000

    Fix syntax error

--- human-activity-recognition.py
+++ human-activity-recognition.py
@@ -30,17 +30,17 @@ with open(r".\UCI HAR dataset\train\X_train.txt", "r") as file:
         '''print(number_tokens)
         training_xs.append([float(x) for x in filter(lambda token: token != "", number_tokens)])'''
 
-'''with open(r".\UCI HAR dataset\train\subject_train.txt", "r") as file:
-    i = 0
-    while True:
-        line = file.readline()
-        if line == "":
-            break
-        assert line[-1] == "\n"
-        line = line[:-1]
-        assert i < len(training_xs)
-        training_xs[i].append(int(line))
-        i += 1'''
+#with open(r".\UCI HAR dataset\train\subject_train.txt", "r") as file:
+#    i = 0
+#    while True:
+#        line = file.readline()
+#        if line == "":
+#            break
+#        assert line[-1] == "\n"
+#        line = line[:-1]
+#        assert i < len(training_xs)
+#        training_xs[i].append(int(line))
+#        i += 1
 
 with open(r".\UCI HAR dataset\train\y_train.txt", "r") as file:
     while True:
