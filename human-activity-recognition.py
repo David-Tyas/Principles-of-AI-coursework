02e81803fedc96e24afc651ee48fceacdeec5769
David-Tyas <David.Tyas@city.ac.uk>
2025-03-21 17:00:58 +0000

    Load testing data

--- human-activity-recognition.py
+++ human-activity-recognition.py
@@ -13,7 +13,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 training_xs = []
+testing_xs = []
 training_ys = []
+testing_ys = []
 
 with open(r".\UCI HAR dataset\train\X_train.txt", "r") as file:
     while True:
@@ -32,6 +34,22 @@ with open(r".\UCI HAR dataset\train\X_train.txt", "r") as file:
         training_xs.append(current_measurements)
         '''print(number_tokens)
         training_xs.append([float(x) for x in filter(lambda token: token != "", number_tokens)])'''
+
+with open(r".\UCI HAR dataset\test\X_test.txt", "r") as file:
+    while True:
+        line = file.readline()
+        if line == "":
+            break
+        assert line[-1] == "\n"
+        #Each line starts with two spaces,
+        #so we strip them off as well as the line feed.
+        line = line[2:-1]
+        number_tokens = line.split(" ")
+        current_measurements = []
+        for token in number_tokens:
+            if token != "":
+                current_measurements.append(float(token))
+        testing_xs.append(current_measurements)
 
 #with open(r".\UCI HAR dataset\train\subject_train.txt", "r") as file:
 #    i = 0
@@ -52,6 +70,14 @@ with open(r".\UCI HAR dataset\train\y_train.txt", "r") as file:
             break
         assert line[-1] == "\n"
         training_ys.append(1 if int(line[:-1]) > 3 else 0)
+
+with open(r".\UCI HAR dataset\test\y_test.txt", "r") as file:
+    while True:
+        line = file.readline()
+        if line == "":
+            break
+        assert line[-1] == "\n"
+        testing_ys.append(1 if int(line[:-1]) > 3 else 0)
 
 pipeline = Pipeline([
     ('scalar', preprocessing.StandardScaler),
